d293d7575d986a024e6ddbc009bbb717549982c4
vewe7 <naomi.yoon.do@gmail.com>
2023-11-19 22:26:45 -0500

    Implement code gen for header and literals

--- gen_code.c
+++ gen_code.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "ast.h"
 #include "bof.h"
 #include "instruction.h"
@@ -9,6 +10,8 @@
 #include "utilities.h"
 #include "regname.h"
 #include "pl0.tab.h"
+
+#define STACK_SPACE 4096
 
 void gen_code_initialize() 
 {
@@ -25,12 +28,38 @@ static void gen_code_output_seq(BOFFILE bf, code_seq cs)
     }
 }
 
+// Return a header appropriate for the give code
+static BOFHeader gen_code_program_header(code_seq main_cs)
+{
+    BOFHeader ret;
+    strncpy(ret.magic, "BOF", MAGIC_BUFFER_SIZE);
+    ret.text_start_address = 0;
+    ret.text_length = code_seq_size(main_cs) * BYTES_PER_WORD;
+    int dsa = MAX(ret.text_length, 1024);
+    ret.data_start_address = dsa;
+    ret.data_length = literal_table_size() * BYTES_PER_WORD;
+    int sba = dsa + ret.data_length + STACK_SPACE;
+    ret.stack_bottom_addr = sba;
+    return ret;
+}
+
+static void gen_code_output_literals(BOFFILE bf)
+{
+    literal_table_start_iteration();
+    while (literal_table_iteration_has_next()) {
+        word_type w = literal_table_iteration_next();
+        // debug_print("Writing literal %f to BOF file\n", w);
+        bof_write_word(bf, w);
+    }
+    literal_table_end_iteration(); // not necessary
+}
+
 void gen_code_output_program(BOFFILE bf, code_seq main_seq)
 {
-    // BOFHeader bfh = gen_code_program_header(main_cs);
-    // bof_write_header(bf, bfh);
+    BOFHeader bfh = gen_code_program_header(main_seq);
+    bof_write_header(bf, bfh);
     gen_code_output_seq(bf, main_seq);
-    // gen_code_output_literals(bf);
+    gen_code_output_literals(bf);
     bof_close(bf);
 }
 
